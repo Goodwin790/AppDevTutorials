bb3495f534140f134fcb1c5be4bc7e94f39b335a
Goodwin790 <goodwin79011@icloud.com>
2024-05-22 11:41:47 +0300

    no message

---
+++ App Dev Tutorials/CreatingANavigationHierarchy/StartingProject/Scrumdinger/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Sergei Kirov on 22.05.2024.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
