78d16caa8383625db8d4c67da4cae576aaaa8562
Goodwin790 <goodwin79011@icloud.com>
2024-05-22 11:43:14 +0300

    no message

--- App Dev Tutorials/CreatingANavigationHierarchy/StartingProject/Scrumdinger/Views/DetailView.swift
+++ App Dev Tutorials/CreatingANavigationHierarchy/StartingProject/Scrumdinger/Views/DetailView.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct DetailView: View {
+    
+    let scrum: DailyScrum
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section(header: Text("Meeting info")) {
+                NavigationLink {
+                    MeetingView()
+                } label: {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                    .foregroundStyle(Color.accentColor)
+                }
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                .accessibilityElement(children: .combine)
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .foregroundStyle(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                }
+                .accessibilityElement(children: .combine)
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            }
+        }
+        .navigationTitle(scrum.title)
     }
 }
 
 #Preview {
-    DetailView()
+    NavigationStack {
+        DetailView(scrum: DailyScrum.sampleData[0])
+    }
 }
